0e6ddf081f38c5203c8d567bc28e8c045d7c25df
dmsgkr02 <dmsgkr02@naver.com>
2023-09-01 01:36:57 +0900

    Feat: Issue Detail 페이지 구현

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import Loader from '../components/common/Loader';
+import IssueItem from '../components/issue/IssueItem';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { fetchIssueDetail } from '../redux/slices/issueDetail';
+import React, { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useParams } from 'react-router';
 
 export default function Detail() {
-  return <div>Detail</div>;
+  const { number } = useParams();
+  const issueNumber = Number(number);
+  const dispatch = useAppDispatch();
+  const { data: issue, loading, error } = useAppSelector(state => state.issueDetail);
+  const { organization, repository } = useAppSelector(state => state.issueOption);
+
+  useEffect(() => {
+    dispatch(fetchIssueDetail({ organization, repository, issueNumber }));
+  }, []);
+
+  if (error) {
+    return <div>error page</div>;
+  }
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <>
+      <IssueItem issue={issue} />
+      <ReactMarkdown children={issue.body} />
+      {issue.body}
+    </>
+  );
 }

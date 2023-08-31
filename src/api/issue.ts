1c4149f1f5c3ac639fa2222f56c412768e68590e
llbllhllk <rkdqudgus9806@gmail.com>
2023-08-31 18:38:03 +0900

    Feat: fetchGetIssues, fetchGetIssueDetail api 추가

--- src/api/issue.ts
+++ src/api/issue.ts
@@ -1,1 +1,47 @@
-export {};
+import { Issue } from '../types';
+import instance from './index';
+
+const fetchGetIssues = async (
+  organization: string,
+  repository: string,
+  page: number,
+): Promise<Issue[]> => {
+  const res = await instance.get(`/repos/${organization}/${repository}/issues`, {
+    params: {
+      sort: 'comments',
+      page: page,
+      per_page: 10,
+    },
+  });
+  return res.data.map((issue: any) => {
+    return {
+      number: issue.number,
+      author: issue.user.login,
+      date: issue.created_at,
+      title: issue.title,
+      comments: issue.comments,
+      image: issue.user.avatar_url,
+      body: issue.body,
+    };
+  });
+};
+
+const fetchGetIssueDetail = async (
+  organization: string,
+  repository: string,
+  issueNumber: number,
+): Promise<Issue> => {
+  const issue = (await instance.get(`/repos/${organization}/${repository}/issues/${issueNumber}`))
+    .data;
+  return {
+    number: issue.number,
+    author: issue.user.login,
+    date: issue.created_at,
+    title: issue.title,
+    comments: issue.comments,
+    image: issue.user.avatar_url,
+    body: issue.body,
+  };
+};
+
+export { fetchGetIssues, fetchGetIssueDetail };

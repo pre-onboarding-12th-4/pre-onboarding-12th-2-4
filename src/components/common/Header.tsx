f1cebf60e07c765f2a0b6f1f22923e127b9c5403
Youme <mrsimplelife@naver.com>
2023-08-31 23:11:02 +0900

    Feat: header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import { RootState } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import { styled } from 'styled-components';
 
 export default function Header() {
-  return <div>Header</div>;
+  const { organization, repository } = useSelector((state: RootState) => state.issueOption);
+  return (
+    <RepoHeader>
+      <RepoOwner>{organization}</RepoOwner>
+      <RepoName>/</RepoName>
+      <RepoName>{repository}</RepoName>
+    </RepoHeader>
+  );
 }
+
+const RepoHeader = styled.div`
+  background-color: #20232a;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  font-family: Arial, sans-serif;
+  font-size: 20px;
+  height: 54.5px;
+  gap: 8px;
+`;
+
+const RepoOwner = styled.span`
+  color: #61dafb;
+`;
+
+const RepoName = styled.span`
+  color: #e1e4e8;
+  font-weight: bold;
+`;
